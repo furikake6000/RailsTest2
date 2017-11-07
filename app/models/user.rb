600ea66759724c25448045e5760764f9c10e1a84
Furikake <ewave63@keio.jp>
2017-11-07 18:25:51 +0900

    Eメールアドレスの正規表現とテストケースの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
+
     validates(:name, presence:true, length: {maximum:255})
-    validates(:email, presence:true, length: {maximum:255})
+    validates(:email, presence:true, length: {maximum:255}, format: { with: VALID_EMAIL_REGEX })
 end
