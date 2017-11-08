d16eb6be31a42523c3c5542eba2929252ded06de
Furikake <ewave63@keio.jp>
2017-11-08 14:00:52 +0900

    Userのemailの一意性を確保

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,49 +1,57 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
-  end
+    def setup
+        @user = User.new(name: "Example User", email: "user@example.com")
+    end
 
-  test "vaildtest" do
-      assert @user.valid?
-  end
+    test "vaildtest" do
+        assert @user.valid?
+    end
 
-  test "vaildtest:nameblank" do
-    @user.name = ""
-    assert_not @user.valid?
-  end
+    test "vaildtest:nameblank" do
+        @user.name = ""
+        assert_not @user.valid?
+    end
 
-  test "vaildtest:emailblank" do
-    @user.email = ""
-    assert_not @user.valid?
-  end
+    test "vaildtest:emailblank" do
+        @user.email = ""
+        assert_not @user.valid?
+    end
 
-  test "vaildtest:nametoolong" do
-    @user.name = "a" * 260
-    assert_not @user.valid?
-  end
+    test "vaildtest:nametoolong" do
+        @user.name = "a" * 260
+        assert_not @user.valid?
+    end
 
-  test "vaildtest:emailtoolong" do
-    @user.email = "a" * 244 + "@example.com"
-    assert_not @user.valid?
-  end
+    test "vaildtest:emailtoolong" do
+        @user.email = "a" * 244 + "@example.com"
+        assert_not @user.valid?
+    end
 
-  test "validtest:valid email addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    test "validtest:valid email addresses" do
+        valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
-    valid_addresses.each do |valid_address|
-      @user.email = valid_address
-      assert @user.valid?, "#{valid_address.inspect} should be valid"
+        valid_addresses.each do |valid_address|
+            @user.email = valid_address
+            assert @user.valid?, "#{valid_address.inspect} should be valid"
+        end
     end
-  end
 
-  test "validtest:invalid email addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org
+    test "validtest:invalid email addresses" do
+        invalid_addresses = %w[user@example,com user_at_foo.org
                            foo@bar_baz.com foo@bar+baz.com]
-    invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+        invalid_addresses.each do |invalid_address|
+            @user.email = invalid_address
+            assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+        end
     end
-  end
+
+    test "validtest:same email addresses" do
+        duplicate_user = @user.dup
+        @user.save
+        assert_not duplicate_user.valid?, "Same address should be invalid"
+        duplicate_user.email = @user.email.upcase
+        assert_not duplicate_user.valid?, "Upcase address should be invalid"
+    end
 end
