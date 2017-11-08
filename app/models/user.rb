01da322feb4f8afb72da9da74688be10e562586a
Furikake <ewave63@keio.jp>
2017-11-08 14:04:13 +0900

    タイプミス

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     validates(:email, 
         presence:true, 
         length: {maximum:255}, 
-        format: { with: VALID_EMAIL_REGEX }
+        format: { with: VALID_EMAIL_REGEX },
         uniqueness: { case_sensitive: false }
         )
 end
