d7b5c58151fb91a6675d9dbf64b7a9e101f5a654
Furikake <ewave63@keio.jp>
2017-11-09 23:10:43 +0900

    WelcomeのFlashを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params())    # 実装は終わっていないことに注意!
     if @user.save
-      # Handle a successful save.
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to(user_url(@user))
     else
       render 'new'
     end
