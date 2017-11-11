226aa19dff78864d018b5a7b3646cf551436777b
Furikake <ewave63@keio.jp>
2017-11-11 18:33:06 +0900

    ログインしっぱい時のflashが更新で消えるように　あとログイン成功時の処理を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
         #ログイン成功
+        log_in(user)
+        redirect_to(user)
     else
         #ログインしっぱい
         if user
             #ユーザがいませんよ
-            flash[:danger] = 'Invalid email'
+            flash.now[:danger] = 'Invalid email'
         else
             #パスが間違ってますよ
-            flash[:danger] = 'Invalid password combination'
+            flash.now[:danger] = 'Invalid password combination'
         end
         #再度ログイン画面を描画
         render 'new'
