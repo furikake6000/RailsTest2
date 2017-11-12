9471f825ec8a14658703191903bfeecc2c393d70
Furikake <ewave63@keio.jp>
2017-11-12 15:55:43 +0900

    複数ブラウザでの多重ログインなどを防止するテストを作成

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -61,4 +61,38 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get(root_path)
     assert(flash.empty?)
   end
+
+  test "login correctly with two browser" do
+    get(login_path)
+    #正しいログイン情報
+    post(login_path, params: {
+      session: {
+        email: @user.email, 
+        password:"password"
+        }})
+
+    assert(is_logged_in?())
+
+    #ページが遷移することを確認
+    assert_redirected_to(@user)
+    follow_redirect!()
+    assert_template('users/show')
+
+    #ログアウト
+    delete(logout_path)
+    assert_not(is_logged_in?())
+    #メインページに戻る
+    assert_redirected_to(root_url)
+
+    #2番目のブラウザでログアウト
+    delete(logout_path)
+    follow_redirect!()
+
+    #ログインリンクがあることを確認
+    assert_select("a[href=?]", login_path)
+    #ログアウトリンクがないことを確認
+    assert_select("a[href=?]", logout_path, count:0)
+    #ユーザプロフへのリンクがないことを確認
+    assert_select("a[href=?]", user_path(@user), count:0)
+  end
 end
