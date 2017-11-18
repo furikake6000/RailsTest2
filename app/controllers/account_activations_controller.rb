8bfc7d667c1d58fc0983b47b24c7dfed519c2c7c
Furikake <ewave63@keio.jp>
2017-11-18 17:22:41 +0900

    アクティベーションの挙動を書く

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,34 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+
+    #存在確認
+    if !user
+      flash[:danger] = "We cannot find such user."
+      redirect_to(root_url)
+    end
+
+    #既に認証されていたら
+    if user.activated?()
+      flash[:danger] = "This user has already activated."
+      redirect_to(root_url)
+    end
+
+    #URLのid(最後につくハッシュ化文字列)とactivation_digestを認証
+    if user.authenticated?(:activation, params[:id])
+      #成功
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in(user)
+
+      flash[:success] = "Account activated"
+      redirect_to(user)
+    else
+      #しっぱい
+      flash[:danger] = "Invalid activation link"
+      redirect_to(root_url)
+    end
+  end
+
 end
