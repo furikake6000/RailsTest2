9d0309ed003431b1b3ec331372ebaadb4b8f8281
Furikake <ewave63@keio.jp>
2017-11-20 13:03:28 +0900

    並び順修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
+  #時間降順に並ぶように
+  default_scope -> {order(created_at: :desc)}
+
   validates :user_id, presence:true
   validates :content, presence:true, length:{maximum: 140}
 end
