5ce77a418e238a9e2eb48bf59ae269ee8ef79031
Furikake <ewave63@keio.jp>
2017-11-21 13:21:31 +0900

    loginしているユーザを取得する関数logged_in_userを全体から使えるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,18 +65,6 @@ class UsersController < ApplicationController
             :password_confirmation)
     end
 
-    #ログイン済みユーザかどうか確認する
-    def logged_in_user()
-      unless logged_in?()
-        #ログインしていなければログインページにリダイレクト
-
-        #リダイレクト先を保存
-        store_location()
-        flash[:danger] = "Please log in."
-        redirect_to(login_url)
-      end
-    end
-
     #正しいユーザかどうか確認する
     def correct_user()
       @user = User.find(params[:id])
