9c88e6683db6baf85a6e638bff1a7fdb6bea87d4
Furikake <ewave63@keio.jp>
2017-11-21 15:30:54 +0900

    画像に関するvalidationを設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,4 +9,10 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence:true
   validates :content, presence:true, length:{maximum: 140}
+  validate :pucture_size, 5.megabytes
+
+  private
+    def picture_size(size)
+      errors.add(:picture, "File is too heavy!") if picture.size > size
+    end
 end
