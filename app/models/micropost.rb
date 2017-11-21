1c6ed34f50d243f4dbc94a8c1cafee42d5918831
Furikake <ewave63@keio.jp>
2017-11-21 15:57:22 +0900

    ピクチャアップローダを設置

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,10 +9,10 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence:true
   validates :content, presence:true, length:{maximum: 140}
-  validate :pucture_size, 5.megabytes
+  validate :picture_size
 
   private
-    def picture_size(size)
-      errors.add(:picture, "File is too heavy!") if picture.size > size
+    def picture_size
+      errors.add(:picture, "File is too heavy!") if picture.size > 5.megabytes
     end
 end
