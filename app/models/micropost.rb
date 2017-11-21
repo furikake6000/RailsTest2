07c2fef894678eb7b20e617102b6ad4a3541cbdd
Furikake <ewave63@keio.jp>
2017-11-21 15:09:27 +0900

    micropost にアップローダを紐付け

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,9 @@ class Micropost < ApplicationRecord
   #時間降順に並ぶように
   default_scope -> {order(created_at: :desc)}
 
+  #アップローダを紐付け
+  mount_uploader :picture, PictureUploader
+
   validates :user_id, presence:true
   validates :content, presence:true, length:{maximum: 140}
 end
